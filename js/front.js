86c83b660d0ec6dca023a0be26045fe97810b6d2
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-08-13 18:06:54 -0400

    Remove unused javascript in front.js

--- js/front.js
+++ js/front.js
@@ -1,24 +1,11 @@
 $(function () {
 
-    /* =========================================
-     * tooltip
-     *  =======================================*/
-
     $('.customer img').tooltip();
-
-
-    /* =========================================
-     * counters
-     *  =======================================*/
 
     $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
-
-    /* =================================================
-     * Preventing URL update on navigation link click
-     *  ==============================================*/
 
     $('.link-scroll').on('click', function (e) {
         var anchor = $(this);
@@ -28,51 +15,11 @@ $(function () {
         e.preventDefault();
     });
 
-
-    /* =========================================
-     *  Scroll Spy
-     *  =======================================*/
-
     $('body').scrollspy({
         target: '#navbarcollapse',
         offset: 80
     });
 
-
-    /* =========================================
-     * testimonial slider
-     *  =======================================*/
-
-    $(".testimonials").owlCarousel({
-        nav: false,
-        dots: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-    });
-
-
-    /* =========================================
-     * google map
-     *  =======================================*/
-    map();
-
-
-    /* =========================================
-     * parallax
-     *  =======================================*/
     $(window).scroll(function () {
 
         var scroll = $(this).scrollTop();
@@ -87,10 +34,6 @@ $(function () {
             });
         }
     });
-
-    /* =========================================
-     * filter
-     *  =======================================*/
 
     $('#filter a').click(function (e) {
         e.preventDefault();
@@ -111,10 +54,6 @@ $(function () {
 
     });
 
-
-    /* =========================================
-     * reference functionality
-     *  =======================================*/
     $('.reference a').on('click', function (e) {
 
         e.preventDefault();
@@ -172,15 +111,10 @@ $(function () {
         closeReference();
     });
 
-
-    /* =========================================
-     *  animations
-     *  =======================================*/
-
     delayTime = 0;
 
     $('[data-animate]').waypoint(function (direction) {
-        delayTime += 250;
+        delayTime += 100;
 
         var element = $(this.element);
 
@@ -194,8 +128,8 @@ $(function () {
         this.destroy();
 
     }, {
-        offset: '90%'
-    });
+            offset: '90%'
+        });
 
     $('[data-animate-hover]').hover(function () {
         $(this).css({
@@ -209,167 +143,4 @@ $(function () {
         $(this).removeClass($(this).data('animate-hover'));
     });
 
-    /* =========================================
-     * for demo purpose
-     *  =======================================*/
-
-    var stylesheet = $('link#theme-stylesheet');
-    $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
-    var alternateColour = $('link#new-stylesheet');
-
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
-
-    $("#colour").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_csspath = 'css/style.' + $(this).val() + '.css';
-
-            alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, {
-                expires: 365,
-                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-            });
-
-        }
-
-        return false;
-    });
-
 });
-
-
-
-/* =========================================
- * styled Google Map
- *  =======================================*/
-
-function map() {
-
-    if ($('#map').length > 0) {
-
-
-        function initMap() {
-
-            var location = new google.maps.LatLng(50.0875726, 14.4189987);
-
-            var mapCanvas = document.getElementById('map');
-            var mapOptions = {
-                center: location,
-                zoom: 16,
-                panControl: false,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(mapCanvas, mapOptions);
-
-            var markerImage = 'img/marker.png';
-
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map,
-                icon: markerImage
-            });
-
-            var contentString = '<div class="info-window">' +
-                '<h3>Info Window Content</h3>' +
-                '<div class="info-content">' +
-                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-                '</div>' +
-                '</div>';
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString,
-                maxWidth: 400
-            });
-
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-
-            var styles = [{
-                "featureType": "landscape",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 65
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "poi",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 51
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.highway",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 30
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "road.local",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 40
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "transit",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "administrative.province",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "labels",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -100
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "hue": "#ffff00"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -97
-                }]
-            }];
-
-            map.set('styles', styles);
-        }
-
-        google.maps.event.addDomListener(window, 'load', initMap);
-    }
-
-}
